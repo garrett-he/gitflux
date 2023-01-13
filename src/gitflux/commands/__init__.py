9fbbe9e25f79c99f2b9e3a39d5d32ca5f78bfc1a
Garrett HE <garrett.he@hotmail.com>
2023-01-13 11:41:02 +0800

    Add command to delete an existing repository

--- src/gitflux/commands/__init__.py
+++ src/gitflux/commands/__init__.py
@@ -1,4 +1,5 @@
 from .list_repos import list_repos_command
 from .create_repo import create_repo_command
+from .delete_repo import delete_repo_command
 
-command_group = [list_repos_command, create_repo_command]
+command_group = [list_repos_command, create_repo_command, delete_repo_command]
