a304baa160725fd9aaea6da56d6627fd72ed4caf
Garrett HE <garrett.he@hotmail.com>
2023-01-18 12:22:58 +0800

    Add multiple providers support for command create-repos

--- src/gitflux/commands/create_repos.py
+++ src/gitflux/commands/create_repos.py
@@ -1,6 +1,5 @@
 import click
-from github import Github
-from gitflux.utils import parse_repo_fullname
+from gitflux.providers import GitServiceProvider
 
 
 @click.command('create-repos')
@@ -10,11 +9,7 @@ from gitflux.utils import parse_repo_fullname
 def create_repos_command(ctx: click.Context, names: tuple[str], **options: dict):
     """Create new repositories."""
 
-    github: Github = ctx.obj['github']
-
-    user = github.get_user()
-    orgs = user.get_orgs()
+    provider: GitServiceProvider = ctx.obj['provider']
 
     for name in names:
-        owner, repo_name = parse_repo_fullname(name, user, orgs)
-        owner.create_repo(repo_name, private=options['private'])
+        provider.create_repo(name, private=options['private'])
