5f1edbfd9a1deed862f92cbb4e06ce2d3ba6d2a3
Garrett HE <garrett.he@hotmail.com>
2023-01-19 11:47:25 +0800

    Add feature to support multiple profiles

--- src/gitflux/__main__.py
+++ src/gitflux/__main__.py
@@ -8,23 +8,33 @@ from gitflux.commands import command_group
 
 @click.group(commands=command_group)
 @click.version_option(message='%(version)s')
-@click.option('--config-file', help='Path of configuration file.', type=click.Path(dir_okay=False), expose_value=True, is_eager=True, default=Path(Path.home(), '.gitfluxrc'))
-@click.option('-p', '--provider', help='Git service provider.', type=click.Choice(['github', 'gitee']), required=False, default='github')
+@click.option('-p', '--profile-name', help='Name of profile to use.', type=str, required=False, default='default')
 @click.pass_context
-def cli(ctx: click.Context, config_file: Path, **options: dict):
-    """A nested command-line utility that helps you manage repositories hosted on Git service providers."""
+def cli(ctx: click.Context, profile_name: str):
+    """A command-line utility that helps you manage repositories hosted on Git service providers."""
 
     ctx.ensure_object(dict)
 
-    config_file = Path(config_file)
+    profile_file = Path().home().joinpath('.config', 'gitflux', 'profiles', profile_name)
 
-    if not config_file.is_file():
-        raise FileNotFoundError(f'Configuration file "{config_file}" not found.')
+    if not profile_file.exists():
+        profile_file.parent.mkdir(parents=True, exist_ok=True)
 
-    provider_module = importlib.import_module(f'gitflux.providers.{options["provider"]}')
+        click.echo(f'Profile "{profile_name}" found, try to generate:')
 
-    config = json.loads(config_file.read_text(encoding='utf-8'))
-    ctx.obj['provider'] = provider_module.create_provider(config['providers'][options['provider']]['accessToken'])
+        profile = {
+            'provider': click.prompt('Provider', type=click.Choice(['github', 'gitee']), default='github'),
+            'access_token': click.prompt('Access token', hide_input=True)
+        }
+
+        profile_file.write_text(json.dumps(profile, ensure_ascii=False, indent=4), encoding='utf-8')
+        profile_file.chmod(0o600)
+    else:
+        profile = json.load(profile_file.open('r', encoding='utf-8'))
+
+    provider_module = importlib.import_module(f'gitflux.providers.{profile["provider"]}')
+
+    ctx.obj['provider'] = provider_module.create_provider(profile['access_token'])
 
 
 if __name__ == '__main__':
