ca62b0484f081415724684ab9928e9e10fb0e2ac
Garrett HE <garrett.he@outlook.com>
2024-06-22 10:28:16 +0800

    Add test for command-line entry

---
+++ tests/test_cli.py
@@ -0,0 +1,14 @@
+import importlib.metadata
+
+from click.testing import CliRunner
+from gitflux.__main__ import cli
+
+
+def test_cli_version(cli_runner: CliRunner):
+    result = cli_runner.invoke(cli, ['--version'])
+
+    if result.exception:
+        print(result.output)
+
+    assert not result.exception
+    assert result.output.strip() == importlib.metadata.version('gitflux')
