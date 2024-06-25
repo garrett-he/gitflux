d3d232cad5cab8802d7f2b1ca1a56301cda0cbc9
Garrett HE <garrett.he@outlook.com>
2024-06-25 11:40:13 +0800

    Add command to create a new repository

--- src/gitflux/commands/__init__.py
+++ src/gitflux/commands/__init__.py
@@ -1,3 +1,4 @@
 from .list_repos import list_repos_command
+from .create_repo import create_repo_command
 
-command_group = [list_repos_command]
+command_group = [list_repos_command, create_repo_command]
