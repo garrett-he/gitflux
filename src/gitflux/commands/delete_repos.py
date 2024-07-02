1717dc54d1099ccb7df1a408b8e1c9028dbaf9f8
Garrett HE <garrett.he@outlook.com>
2024-07-02 10:37:48 +0800

    Support deleting repositories by prefix

--- src/gitflux/commands/delete_repos.py
+++ src/gitflux/commands/delete_repos.py
@@ -4,16 +4,33 @@ from gitflux.utils import parse_repo_fullname
 
 
 @click.command('delete-repos')
-@click.argument('names', required=True, nargs=-1)
+@click.argument('names', required=False, nargs=-1)
+@click.option('--prefix', help='Prefix of repositories to delete.', type=str, default=None, required=False)
 @click.pass_context
-def delete_repos_command(ctx: click.Context, names: tuple[str]):
+def delete_repos_command(ctx: click.Context, names: tuple[str], **options: dict):
     """Delete existing repositories."""
 
     github: Github = ctx.obj['github']
 
     user = github.get_user()
-    orgs = user.get_orgs()
 
-    for name in names:
-        owner, repo_name = parse_repo_fullname(name, user, orgs)
-        owner.get_repo(repo_name).delete()
+    if options['prefix'] is None:
+        orgs = user.get_orgs()
+
+        for name in names:
+            owner, repo_name = parse_repo_fullname(name, user, orgs)
+            owner.get_repo(repo_name).delete()
+    else:
+        if options['prefix'].lower() == 'all':
+            if click.prompt(f'Delete ALL repositories of user: {user.login}, sure?', default='n').lower() == 'n':
+                ctx.exit()
+
+            repos = user.get_repos()
+        else:
+            if click.prompt(f'Delete ALL repositories with prefix: {options["prefix"]}, sure?', default='n').lower() == 'n':
+                ctx.exit()
+
+            repos = filter(lambda repo: repo.owner.login == options['prefix'], user.get_repos())
+
+        for repo in repos:
+            repo.delete()
